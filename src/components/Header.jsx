f47e801b7dcf4080684e2767908c1c0e359e9d2a
Abdo-Ahmed-Mostafa <abdoahmedd20240@gmail.com>
2024-06-30 23:53:47 +0300

    finle

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,7 +7,7 @@ function Header() {
     <div className="header">
       <h1 className="logo">Weather App</h1>
       <span className="credit">
-        <a href="https://github.com/talhashah-dev/weather-app-react" target="_blank" rel="noreferrer"><img src={github_icon} alt="" /></a>
+        <a href="https://github.com/Abdo-Ahmed-Mostafa/theWithert" target="_blank" rel="noreferrer"><img src={github_icon} alt="" /></a>
       </span>      
     </div>
   )
