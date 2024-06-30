31cdea0dcd83e4678ec36b23b8230a4ec61b5d3a
Abdo-Ahmed-Mostafa <abdoahmedd20240@gmail.com>
2024-06-30 23:50:39 +0300

    first

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Weather from "./components/Weather";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Weather />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <div className="footer">
+        <span>
+            <p>Developed By</p>
+            <a href="https://www.linkedin.com/in/abdelrhman-ahmedd/" target="_blank" rel="noreferrer">&lt;Abdelrhman /&gt;</a>  
+        </span>      
+    </div>
+  )
+}
+
+export default Footer;
